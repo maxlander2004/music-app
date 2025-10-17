d6b0da7b668a1abb3a6860c4693d19c619350119
maxlander2004 <akindediekola67@gmail.com>
2025-10-17 08:32:59 +0100

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"68f1f0c190a2bb0024b9d87c",
+				"68f1f0c190a2bb0024b9d87f"
+			]
+		},
+		"68f1f0c190a2bb0024b9d87c": {
+			"id": "68f1f0c190a2bb0024b9d87c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"68f1f0c190a2bb0024b9d87f": {
+			"id": "68f1f0c190a2bb0024b9d87f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
